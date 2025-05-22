d08f2b43f04de963d06354b499e36e7ce36788cc
sujan07 <sujanpokharel704@gmail.com>
2025-05-22 09:24:28 +0545

    initial

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,6 +2,7 @@ const Home=()=>{
     return(
         <>
         <h1>this is home</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam sit beatae, ex aut esse corporis ad. Dolorem ipsam quia omnis asperiores, esse molestiae inventore velit quasi sequi! Architecto consequatur sapiente, nisi quas ducimus voluptate cum. Possimus reiciendis modi, aspernatur aut quibusdam expedita nulla earum a velit cum quisquam eum optio officia tenetur qui. Dolore nulla obcaecati sit ad corporis exercitationem cum, corrupti atque alias, velit quos sequi necessitatibus maxime blanditiis accusantium sapiente in explicabo. Unde accusamus excepturi voluptatibus fugit ipsa recusandae natus eius esse voluptates, atque, tenetur voluptate? Nobis, perspiciatis?</p>
         </>
     )
 }
