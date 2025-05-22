804bde793fa237ecd3837ae0931efb2b8879e80d
BishalParajuli4 <124025748+BishalParajuli4@users.noreply.github.com>
2025-05-22 12:24:15 +0545

    Initial

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,6 +2,8 @@ const Home=()=>{
     return(
         <>
         <h1>this is home</h1>
+        <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis minus ullam rerum ratione, quia nam officia placeat possimus repellat sed sequi provident praesentium perspiciatis fuga expedita a voluptatem quam alias eos dolor consectetur sunt laudantium et! Dicta quam suscipit amet officiis cupiditate vero totam eligendi error nisi impedit nostrum saepe quaerat magni ipsum dolorum, maxime facilis consequatur quae assumenda dolorem sed adipisci. Soluta iusto at ipsum officiis, magni quibusdam iste tempora labore dolor sunt deserunt perferendis error, libero hic, iure consequuntur perspiciatis! Omnis quidem dolor laborum! Laudantium distinctio aliquid quos ipsum dignissimos harum nemo possimus voluptatibus optio ad! Neque?
+        </h3>
         </>
     )
 }
